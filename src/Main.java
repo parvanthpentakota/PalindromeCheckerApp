a5d1b173f8d85baee78306e97d38b0cf1b7a4955
Parvanth Pentakota <parvanthpentakota2316@gmail.com>
2026-03-08 13:54:47 +0530

    UC6 - Palindrome check using Queue and Stack

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
 public class Main {
+
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println("     Palindrome Checker App");
-        System.out.println("           Version 1.0");
-        System.out.println("=================================");
+        String input = "civic";
 
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // Insert characters into both structures
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+            queue.add(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare FIFO and LIFO outputs
+        while (!queue.isEmpty()) {
+
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Required output format
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }

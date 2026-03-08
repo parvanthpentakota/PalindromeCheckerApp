26819150b2654aa9f609f5a43d2773e2c1d9219e
Parvanth Pentakota <parvanthpentakota2316@gmail.com>
2026-03-08 14:36:53 +0530

    UC13 - Performance comparison using System.nanoTime

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String input = "Level";
+        String input = "level";
 
         // Choose strategy
         PalindromeStrategy strategy = new StackStrategy();
         // PalindromeStrategy strategy = new DequeStrategy();
 
+        long startTime = System.nanoTime();
+
         boolean result = strategy.check(input);
+
+        long endTime = System.nanoTime();
+
+        long executionTime = endTime - startTime;
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + result);
+        System.out.println("Execution Time : " + executionTime + " ns");
     }
 }

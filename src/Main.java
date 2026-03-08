fbe27f3822053559005bf09f875762497a64d0a1
Parvanth Pentakota <parvanthpentakota2316@gmail.com>
2026-03-08 13:19:10 +0530

    UC1 implementation

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println("     Palindrome Checker App");
-        System.out.println("           Version 1.0");
-        System.out.println("=================================");
+        System.out.println("Welcome to the Palindrome Checker App Management System");
 
     }
+
 }

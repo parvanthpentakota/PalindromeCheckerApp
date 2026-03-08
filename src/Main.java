be848200dbd0e645ac46f482a2fa6f24838359c2
Parvanth Pentakota <parvanthpentakota2316@gmail.com>
2026-03-08 12:43:17 +0530

    Initial commit - Palindrome Checker App

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("     Palindrome Checker App");
+        System.out.println("           Version 1.0");
+        System.out.println("=================================");
+
+    }
+}
